ff44d7f86b9b23c0092be7d76dad556c80e0837f
root <root@249cc.yeg.cybera.ca>
2019-12-04 22:24:04 +0000

    Cahnges in the ce

--- ce/add_ce_users.sh
+++ ce/add_ce_users.sh
@@ -10,4 +10,7 @@ groupadd -g 3003 glow && useradd -u 3003 -g 3003 -s /bin/bash glow
 groupadd -g 3004 ligo && useradd -u 3004 -g 3004 -s /bin/bash ligo
 groupadd -g 3005 hcc && useradd -u 3005 -g 3005 -s /bin/bash hcc
 groupadd -g 3006 cms && useradd -u 3006 -g 3006 -s /bin/bash cms
+groupadd -g 3006 cmsuser && useradd -u 3006 -g 3006 -s /bin/bash cmsuser
+groupadd -g 3006 cmspilot && useradd -u 3006 -g 3006 -s /bin/bash cmspilot
+groupadd -g 3006 fermigli && useradd -u 3006 -g 3006 -s /bin/bash fermigli
 
